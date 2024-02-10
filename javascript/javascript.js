3af38be744ec4cec58c5f58d24469007bf4d4002
jerricktlee <83623094+jerricktlee@users.noreply.github.com>
2024-02-10 14:38:07 -0800

    Finish html and styling

---
+++ javascript/javascript.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+const addBtn = document.querySelector('#add-btn');
+
+function Book() {
+
+}
+
+function addBookToLibrary() {
+
+}
+
+addBtn.addEventListener('click', () => {
+   alert('Add button clicked');
+});
